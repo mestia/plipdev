16127864982c64696ec4bd5671555b272472b3fc
Shanya <singhal@pharm.ai>
2025-05-08 14:05:07 +0200

    fix: condition for as_string in read_pdb

--- plip/structure/preparation.py
+++ plip/structure/preparation.py
@@ -1448,7 +1448,7 @@ class PDBComplex:
 
         if not as_string:
             self.sourcefiles['filename'] = os.path.basename(self.sourcefiles['pdbcomplex'])
-        self.protcomplex, self.filetype = read_pdb(self.corrected_pdb, as_string= (self.corrected_pdb != pdbpath)) # self.corrected_pdb may fallback to pdbpath
+        self.protcomplex, self.filetype = read_pdb(self.corrected_pdb, as_string= as_string or pdbparser.pdb_file_was_corrected) # self.corrected_pdb may fallback to pdbpath
 
         # Update the model in the Mapper class instance
         self.Mapper.original_structure = self.protcomplex.OBMol
